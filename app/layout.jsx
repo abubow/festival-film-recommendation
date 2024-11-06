33d28f0290dccfa81927d00eb431e1f5065bf43d
Syed Mohammad Abuzar <sabuzar00@gmail.com>
2024-11-07 03:02:05 +0500

    fixed links and components

--- app/layout.jsx
+++ app/layout.jsx
@@ -14,7 +14,7 @@ const geistMono = localFont({
 });
 
 export const metadata = {
-  title: "HottuDogo",
+  title: "HottoDogu",
   description: "Happy birthday to the cutest person in the world!",
 };
 
